4381e5f7c3bd4b39d95379a1d3fa447a652920e0
jalibhai <jalibhai5526@gmail.com>
2024-10-25 15:38:02 +0100

    Remove all initialising functions that will be handled by the AWS CDK

--- flask/start.sh
+++ flask/start.sh
@@ -4,17 +4,4 @@ ngrok config add-authtoken $NGROK_TOKEN
 
 # Start the server
 ngrok http --domain=$NGROK_DOMAIN 5000 &
-flask run --host=0.0.0.0 --port=5000 &
-
-# Wait for the server to start
-count=0
-while [ $count -lt 10 ]; do
-  curl -s http://localhost:5000/health && break
-  count=$((count + 1))
-  sleep 2
-done
-
-# Initialise the server
-curl -X POST http://localhost:5000/initialise || true
-
-wait
+flask run --host=0.0.0.0 --port=5000
